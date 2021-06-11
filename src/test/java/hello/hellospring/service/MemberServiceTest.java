caafcac7b398f3cc823c09247b00d7e45beefef3
GyeongEun Kim <kekim20@naver.com>
2021-06-11 21:59:28 +0900

    테스트 케이스 복습

--- src/test/java/hello/hellospring/service/MemberServiceTest.java
+++ src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -12,34 +12,40 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.fail;
+
 public class MemberServiceTest {
 
-    MemberService memberService ;
+    MemberService memberService;
     MemoryMemberRepository memberRepository;
 
     @BeforeEach
-    public void beforeEach () {
+    public void beforeEach() {
         memberRepository = new MemoryMemberRepository();
         memberService = new MemberService(memberRepository);
     }
-    @AfterEach
-    public void clear (){
 
+    @AfterEach
+    public void clear() {
+        memberRepository.clearStore();
     }
+
     @Test
-    public void 회원가입 (){
+    public void 회원가입() {
         //given
         Member member = new Member();
         member.setName("이름");
         //when
         long saveId = memberService.join(member);
-
         //then
         Member findMember = memberService.findOne(saveId).get();
-        Assertions.assertThat(member.getName()).isEqualTo(findMember.getName());
+        assertThat(member.getName()).isEqualTo(findMember.getName());
     }
 
-    public void 중복회원예외 () {
+    @Test
+    public void 중복회원예외() {
         //given
         Member member1 = new Member();
         member1.setName("spring1");
@@ -50,9 +56,10 @@ public class MemberServiceTest {
 
         //when
         memberService.join(member1);
-        memberService.join(member2);
         //then
-
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다");
+        }
     }
 
-}
+
